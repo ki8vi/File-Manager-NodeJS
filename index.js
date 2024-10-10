079b47387a0b8493ef309a220813afe20ac6f4b2
ki8vi <oneofbln@gmail.com>
2024-10-10 11:34:36 +0500

    feat: implement UsernameParser class

---
+++ index.js
@@ -0,0 +1,5 @@
+import UsernameParser from './src/usernameParser/index.js';
+const username = new UsernameParser();
+console.log(username.getUsername());
+
+
---
+++ src/usernameParser/index.js
@@ -0,0 +1,39 @@
+export default class UsernameParser {
+    constructor() {
+        this.username = null;
+    }
+
+    #parseArgv() {
+        const args = process.argv.slice(2);
+        if (args.length) {
+            const parsedUsername = args[0].split('=')[1];
+            if (parsedUsername) {
+                this.username = parsedUsername;
+            }
+        }
+    }
+
+    #parseEnv() {
+        if (!this.username) {
+            const envUsername = process.env.npm_config_username;
+            if (envUsername) {
+                this.username = envUsername;
+            }
+        }
+    }
+
+    
+    #uppercasedUsername() {
+        if (this.username) {
+            this.username = this.username.slice(0, 1).toUpperCase().concat(this.username.slice(1));
+        }
+    }
+
+    getUsername() {
+        this.#parseArgv();
+        this.#parseEnv();
+        this.#uppercasedUsername();
+        return this.username || 'Nameless buddy';
+    }
+}
+
